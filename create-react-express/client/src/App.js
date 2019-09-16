124dffa7f2e9fe165f5b10974f37aff0ebf4cd37
Brookee <oliver.brooke.a@gmail.com>
2019-09-16 17:18:28 -0400

    npm audit fix, running router/local

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from "./logo.svg";
+import Search from "./pages/search";
+import Saved from "./pages/saved";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -8,10 +12,15 @@ class App extends Component {
     return (
       <Router>
         <div>
-          
+          <Nav />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+          </Wrapper>
         </div>
       </Router>
-    )
+    );
   }
 }
 
